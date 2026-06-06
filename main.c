be4880f3f12efa7f19e9c18393b2600b67b00bd3
martindiazm <martin.diaz.m@mail.pucv.cl>
2026-06-06 16:09:51 -0400

    tdas

--- main.c
+++ main.c
@@ -1,15 +1,74 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "tdas/list.h"
+#include "tdas/heap.h"
+#include "tdas/extra.h"
+#include "tdas/stack.h"
+#include "tdas/queue.h"
+#include <string.h>
+#include <time.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
-#include <stdio.h>
-#include <stdlib.h>
+// Definición de los structs 
 
-int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
+
+
+
+
+
+
+
+// Definición de las funciones 
+
+
+
+
+
+int main()
+{
+    char opcion;
+    do 
+    {
+        
+        puts("========================================");
+        puts("     TRANSPORTE INTELIGENTE ");
+        puts("========================================");
+
+        puts("1) Buscar ruta");
+        puts("2) Gestionar lugares");
+        puts("3) Gestionar conexiones");
+        puts("4) Mostrar red de transporte");
+        puts("5) Salir");
+
+        printf("Ingrese su opción: ");
+        scanf(" %c", &opcion);
+
+        switch (opcion) {
+        case '1':
+          
+          break;
+        case '2':
+          
+          break;
+        case '3':
+          
+          break;
+        case '4':
+            
+            break;
+        
+
+        
+        if (opcion != '5') 
+        {
+            printf("\n");
+            presioneTeclaParaContinuar();
+            limpiarPantalla();
+        }
+
+  } while (opcion != '5');
+
+  return 0;
+
 }
+
