bbae92a7afb09e2201d79ad21263a16ef3dcf848
martindiazm <martin.diaz.m@mail.pucv.cl>
2026-06-06 15:39:59 -0400

    aaaa

---
+++ main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
